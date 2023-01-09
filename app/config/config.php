fa328b4bb7a0ad1ed3a6147c67fbd8a95747b307
DongqiJia <95073122+DongqiJia@users.noreply.github.com>
2023-01-09 11:07:42 +0100

    hele templete toets

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+   // Database constanten
+   define('DB_HOST', 'localhost');
+   define('DB_USER', 'root');
+   define('DB_PASS', '');
+   define('DB_NAME', 'mvc-jaar2-p1');
+   
+   // Het pad naar de app map
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // De URL van het de site
+    define('URLROOT', 'http://www.mvc-jaar2-p1.org');
+    
+    // Naam van de site
+    define('SITENAME', 'MVC-Jaar2-p1');
+?>
---
+++ app/views/landingpages/index.php
@@ -0,0 +1,4 @@
+<h3><?= $data['title']; ?></h3>
+<a href="<?= URLROOT . '/Lessen/index'; ?>">Overzicht Rijlessen</a> <br>
+<a href="<?= URLROOT . '/Countries/index'; ?>">Countries</a> <br>
+<a href="<?= URLROOT . '/Wagenpark/index'; ?>">Wagenpark</a>
