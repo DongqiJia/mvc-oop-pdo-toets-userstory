a49bedf3f5e5762f069cf18b758cfedd81f94e6a
DongqiJia <95073122+DongqiJia@users.noreply.github.com>
2023-01-09 11:33:39 +0100

    landingage link toevoegen

--- app/views/landingpages/index.php
+++ app/views/landingpages/index.php
@@ -1,4 +1,5 @@
 <h3><?= $data['title']; ?></h3>
 <a href="<?= URLROOT . '/Lessen/index'; ?>">Overzicht Rijlessen</a> <br>
 <a href="<?= URLROOT . '/Countries/index'; ?>">Countries</a> <br>
-<a href="<?= URLROOT . '/Wagenpark/index'; ?>">Wagenpark</a>
+<a href="<?= URLROOT . '/Wagenpark/index'; ?>">Wagenpark</a> <br>
+<a href="<?= URLROOT . '/Mankement/index'; ?>">Mankement</a>
